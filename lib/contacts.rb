fba21baa26132dcc4f05227064c2d67d7aceb773
Anuj Panchal <anujpan1@gmail.com>
2018-10-07 00:12:30 +0000

    Done.

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -15,5 +15,12 @@ require 'pry'
   # }
   
 def remove_strawberry(contacts)
-
+  contacts.each do |person, details|
+    
+    details.each do |attr, data|
+      if attr == :favorite_ice_cream_flavors
+        data.delete_if {|ice_cream| ice_cream == "strawberry"}
+      end
+    end
+  end
 end
